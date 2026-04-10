c173ba066bdfd1e34f82ea808f80e92bc81b3f43
adzafic25-cloud <adzafic25@gmail.com>
2026-04-10 02:36:46 +0200

    Update Main.java

--- src/financeapp/Main.java
+++ src/financeapp/Main.java
@@ -4,14 +4,12 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        // SwingUtilities.invokeLater osigurava da se GUI pokrene na ispravan način
         SwingUtilities.invokeLater(() -> {
             try {
-                // Opcionalno: postavlja izgled aplikacije da liči na Windows prozor
+                
                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
-                // Ovdje pokrećemo LOGIN prozor.
-                // To je sada jedina stvar koju Main radi.
+            
                 new LoginForm();
 
             } catch (Exception e) {
